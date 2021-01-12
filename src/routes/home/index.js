e9e27d015fa7df79d66adad4611a2350a7616627
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2021-01-12 22:40:36 +0700

    :sparkles: - set default state on connect

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -12,7 +12,7 @@ const Home = () => {
   const [checked, setChecked] = useState(false)
   const [userId, setUserId] = useState("userid")
   useEffect( () => {
-      const ref = db.collection("user").doc(userId);
+      let ref = db.collection("user").doc(userId);
       if (localStorage.getItem("userID") != null) {
         setUserId(localStorage.getItem("userID")) 
         console.log('localstorage exist')
@@ -20,22 +20,14 @@ const Home = () => {
       else {
         localStorage.setItem("userID", Math.floor(Math.random() * 900000) + 100000);
         setUserId(localStorage.getItem("userID"))
-        db.collection("user").doc(userId).set({ check: checked })
+        ref.set({ check: checked })
         console.log('localstorage dont exist')
       }
-      ref.get().then((doc)=> {
-        if(doc.exists) {
-          setChecked(doc.data().check) 
-          console.log(doc.exists)
-        }
-        else {
-          console.log('doc dont exist')
-          db.collection("user").doc(userId).set({ check: checked })
-        }
-      });
+      ref.set({ check: checked })
     }, [])
   const handleChange = (e) => {
     db.collection("user").doc(userId).set({ check:  e.target.checked})
+    setChecked(e.target.checked)
   }
   return (
     <Fragment>
