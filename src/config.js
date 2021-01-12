5b4ce8a0fe3466b042955d3d02890ef21b5b909d
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2021-01-12 19:08:39 +0700

    :sparkles: - add config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId,
+  apiKey: "AIzaSyBRsko4K3jHwgFGfticAj1y6UmYO7du-kc",
+  authDomain: "meetmic-e6487.firebaseapp.com",
+  projectId: "meetmic-e6487",
+  storageBucket: "meetmic-e6487.appspot.com",
+  messagingSenderId: "569391082173",
+  appId: "1:569391082173:web:cbe0b3af541f9f83b9e5d4",
+  measurementId: "G-RB5SRTZG0B",
 };
