d5e5db3cd90489f4efc48b0543c41ceb971979fe
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2021-01-15 16:35:50 +0700

    return on getItem properly

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,39 +1,61 @@
 import { Fragment, h } from "preact";
-import { useState, useEffect } from "preact/hooks"
+import { useState, useEffect } from "preact/hooks";
+import { setItem, getItem } from "@wit03/async-localstorage";
 
 // eslint-disable-next-line no-unused-vars
 import style from "./style.css";
 
-import firebase from '../../components/firebase'
-import 'firebase/firestore'
+import firebase from "../../components/firebase";
+import "firebase/firestore";
 
 const Home = () => {
   let db = firebase.firestore();
-  const [checked, setChecked] = useState(false)
-  const [userId, setUserId] = useState("userid")
-  useEffect( () => {
-      let ref = db.collection("user").doc(userId);
-      if (localStorage.getItem("userID") != null) {
-        setUserId(localStorage.getItem("userID")) 
-        console.log('localstorage exist')
-      }
-      else {
-        localStorage.setItem("userID", Math.floor(Math.random() * 900000) + 100000);
-        setUserId(localStorage.getItem("userID"))
-        ref.set({ check: false })
-        console.log('localstorage dont exist')
-      }
-      ref.set({ check: false })
-    }, [])
+  const [checked, setChecked] = useState(false);
+  const [userId, setUserId] = useState("userid");
+  useEffect(() => {
+    if (localStorage.getItem("userID") === null) {
+      console.log("localstorage not exist:");
+      setItem("userID", Math.floor(Math.random() * 900000) + 100000)
+        .then(() => {
+          return getItem("userID");
+        })
+        .then((value) => {
+          setUserId(value);
+          db.collection("user")
+            .doc(value)
+            .set({ check: false })
+            .then(() => {
+              console.log("Document successfully written!");
+            });
+        });
+    } else {
+      console.log("localstorage exist: ");
+      getItem("userID").then((value) => {
+        console.log(value);
+        setUserId(localStorage.getItem("userID"));
+        db.collection("user")
+          .doc(value)
+          .set({ check: false })
+          .then(() => {
+            console.log("Document successfully written!");
+          });
+      });
+    }
+  }, []);
   const handleChange = (e) => {
-    db.collection("user").doc(userId).set({ check:  e.target.checked})
-    setChecked(e.target.checked)
-  }
+    db.collection("user").doc(userId).set({ check: e.target.checked });
+    setChecked(e.target.checked);
+  };
   return (
     <Fragment>
       <h1>Tap to mute</h1>
-      <input type="checkbox" id="switch" onChange={handleChange} checked={checked} />
-	  <label for="switch">Toggle</label>
+      <input
+        type="checkbox"
+        id="switch"
+        onChange={handleChange}
+        checked={checked}
+      />
+      <label for="switch">Toggle</label>
       <h3 class={style.id}>Your ID: {userId}</h3>
     </Fragment>
   );
